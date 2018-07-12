0af49536bb4f3c7c9fdfc497793f87b3f1356f02
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-07-12 15:41:13 +0200

    first commit

--- scripts.js
+++ scripts.js
@@ -16,17 +16,19 @@
  }
 
 
-
+ const filmSelector = document.querySelector('#film-select');
+ const roomSelector = document.querySelector('#room-select');
+ const priceSelector = document.querySelector('#price-select');
 
  fetch(request(API_URL + "films", 'GET'))
      .then(res => res.json())
      .then(films => {
          console.log(films);
-         const filmSelector = document.querySelector('#film-select');
+
 
          for (const film of films) {
              const opt = document.createElement("option");
-             opt.value=film.id;
+             opt.value = film.id;
              opt.text = `ID: ${film.id} || Film: ${film.title} || Director: ${film.director} || Genre: ${film.genre} || Length: ${film.length}`;
              filmSelector.options.add(opt);
          }
@@ -40,11 +42,11 @@
      .then(res => res.json())
      .then(rooms => {
          console.log(rooms);
-         const roomSelector = document.querySelector('#room-select');
+
 
          for (const room of rooms) {
              const opt = document.createElement("option");
-             opt.value=room.id;
+             opt.value = room.id;
              opt.text = `${room.id} Seats: ${room.seats}`;
              roomSelector.options.add(opt);
          }
@@ -56,20 +58,20 @@
      .then(res => res.json())
      .then(prices => {
          console.log(prices);
-         const priceSelector = document.querySelector('#price-select');
+
 
          for (const price of prices) {
              const opt = document.createElement("option");
-             opt.value=price.id;
+             opt.value = price.id;
              opt.text = `${price.id} Normal: ${price.normal} Discount: ${price.discount}`;
              priceSelector.options.add(opt);
          }
      });
 
 
-     const showingCreate=document.querySelector('#showing-create');
+ const showingCreate = document.querySelector('#showing-create');
 
-     showingCreate.addEventListener('submit', function(e) {
-        e.preventDefault();
-console.log(this);
-     });
+ showingCreate.addEventListener('submit', function(e) {
+     e.preventDefault();
+     console.log('lawl');
+ });
