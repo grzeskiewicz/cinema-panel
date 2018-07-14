67e7da89b584d9e2aaebf1cc80cc518bd6e9d324
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-07-14 16:09:08 +0200

    Adding calendar

--- scripts.js
+++ scripts.js
@@ -32,7 +32,7 @@
                          day2.classList.remove('date-clicked');
                      }
                  }
-document.querySelector('#date-cal').innerHTML= moment(pickedDate).format();
+document.querySelector('#date-cal').innerHTML= moment(pickedDate).format('DD-MM-YYYY');
 
              });
          }
