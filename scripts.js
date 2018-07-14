989c57010e6aa9e65deb3d51faaa8180659bdbaf
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-07-14 23:18:00 +0200

    Adding calendar

--- scripts.js
+++ scripts.js
@@ -122,6 +122,8 @@ document.querySelector('#date-cal').innerHTML= moment(pickedDate).format('DD.MM.
 
  showingCreate.addEventListener('submit', function(e) {
      e.preventDefault();
+     const time=document.querySelector('appt-time');
+     console.log(time.value);
      const showing = {
          film: filmSelector.value,
          price: priceSelector.value,
