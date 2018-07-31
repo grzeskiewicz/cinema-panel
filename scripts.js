cd2ae85c68e9dacbe1ecda169b45fa10543f3ffc
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-07-31 22:55:25 +0200

    Editing sorting showings by date

--- scripts.js
+++ scripts.js
@@ -142,7 +142,7 @@
              filmTitles.addEventListener('click', function() {
 showingsListed.innerHTML="";
 const result=sortShowings(groupArr[film]);
-console.log(result;
+console.log(result);
                  for (const showing of groupArr[film]) {
                      const descriptionDiv = document.createElement("div");
                      const description = document.createElement("p");
