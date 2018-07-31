a24bd10b6ae213344c1a33ea5f7a41f25b1bd4f8
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-08-01 00:27:04 +0200

    Editing sorting showings by date

--- scripts.js
+++ scripts.js
@@ -332,7 +332,7 @@
      for (const showing of showingsList) {
 
          if (moment(showing.date).format('DD.MM.YYYY') === moment(pickedDate).format('DD.MM.YYYY') && showing.room ===roomSelector.value) {
-            console.log('hehe');
+            console.log(showing.length);
          }
      }
      /*  var firstLetter = form.name.value[0];
