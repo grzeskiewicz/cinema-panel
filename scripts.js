3ec886a5972ebf2ca64e123cd1851d7393c6f8ce
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-08-01 15:32:04 +0200

    Validating dates showings

--- scripts.js
+++ scripts.js
@@ -333,7 +333,7 @@
      for (const showing of showingsList) {
 
          if (moment(showing.date).format('DD.MM.YYYY') === moment(pickedDate).format('DD.MM.YYYY') && showing.room ===roomSelector.value) {
-            console.log(moment(showing.date,'HH:mm').format('HH'));
+            console.log(moment(showing.date));
             console.log(moment(showing.date,'HH:mm').get('hour'),momentTime.get('hour'),showing.id);
          }
      }
