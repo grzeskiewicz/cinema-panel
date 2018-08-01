197325c144d9ddc0a337a175a2d3cbd8c9f75afc
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-08-01 11:29:03 +0200

    Validating dates showings

--- scripts.js
+++ scripts.js
@@ -332,7 +332,7 @@
      for (const showing of showingsList) {
 
          if (moment(showing.date).format('DD.MM.YYYY') === moment(pickedDate).format('DD.MM.YYYY') && showing.room ===roomSelector.value) {
-            console.log(showing.length,(moment(showing.date,'HH:mm')-momentTime),showing.id);
+            console.log(showing.length,moment(showing.date,'HH:mm')-momentTime,showing.id);
          }
      }
      /*  var firstLetter = form.name.value[0];
