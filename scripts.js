e67bdce8558ab39285ef279b29c6a765158aca8b
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-08-07 12:45:27 +0200

    showings from the past are not shown

--- scripts.js
+++ scripts.js
@@ -109,7 +109,7 @@ getShowings().then(res=>{console.log(res)});
          const groupArr = [];
          const showingsListed = document.createElement('div');
          showingsListed.id = "showings-listed";
-         const today=moment('DD.MM.YYYY');
+         const today=moment();
          console.log(today);
          for (const showing of showings) {
              if (groupArr[showing['title']] === undefined) groupArr[showing['title']] = [];
