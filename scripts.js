adcb6061d818596398eb3a161511d02bb61f7746
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-08-14 23:46:19 +0200

    Editing upload

--- scripts.js
+++ scripts.js
@@ -507,11 +507,50 @@ getCustomers();
      return true;
  }
 
+function uploadFile(file, signedRequest, url){
+  const xhr = new XMLHttpRequest();
+  xhr.open('PUT', signedRequest);
+  xhr.onreadystatechange = () => {
+    if(xhr.readyState === 4){
+      if(xhr.status === 200){
+        document.getElementById('preview').src = url;
+        document.getElementById('avatar-url').value = url;
+      }
+      else{
+        alert('Could not upload file.');
+      }
+    }
+  };
+  xhr.send(file);
+}
+
+
+ function getSignedRequest(file){
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', `/sign-s3?file-name=${file.name}&file-type=${file.type}`);
+  xhr.onreadystatechange = () => {
+    if(xhr.readyState === 4){
+      if(xhr.status === 200){
+        const response = JSON.parse(xhr.responseText);
+        uploadFile(file, response.signedRequest, response.url);
+      }
+      else{
+        alert('Could not get signed URL.');
+      }
+    }
+  };
+  xhr.send();
+}
+
+
  const filmCreate = document.querySelector('#film-create');
 
  filmCreate.addEventListener('submit', function(e) {
      e.preventDefault();
-     console.log('heheheh!!!!');
+
+         const files = document.getElementById('file-input').files;
+    const file = files[0];
+         console.log(file);
   /*   console.log(filmCreate.imageUrl.value);
      filmCreate.querySelector('button').disabled = true;
      const film = {
