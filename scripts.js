a3f5277294e4a9be0e795801c42947432753c513
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-08-17 01:07:20 +0200

    Upload

--- scripts.js
+++ scripts.js
@@ -556,7 +556,7 @@
      const file = files[0];
      console.log(IMAGE_URL+file.name);
 getSignedRequest(file);
-const fileUrl=IMAGE_RUL+file.name;
+const fileUrl=IMAGE_URL+file.name;
      /*   console.log(filmCreate.imageUrl.value);
         filmCreate.querySelector('button').disabled = true;
         const film = {
