66341021e47757a240223f26f707adc9a56c3905
grzeskiewicz <ariel.grzeskiewicz@gmail.com>
2018-10-04 10:55:50 +0200

    Editing time selection

--- scripts.js
+++ scripts.js
@@ -87,7 +87,9 @@
          for (let i = 0; i <= 55; i = i + 5) {
              const option = document.createElement("option");
              option.value = i;
-             if (i == 0) { option.text = '00'; } else {
+             if (i == 0) { option.text = '00'; } else if (i == 5) {
+                 option.text = '05';
+             } else {
                  option.text = i;
              }
              minuteSelector.options.add(option);
